c0789ca13cd6deab64e7828800e831cd6d43d837
luoyantao99 <49816287+luoyantao99@users.noreply.github.com>
2023-04-28 12:08:00 -0400

    Update KademliaSimulation.py

--- KademliaSimulation.py
+++ KademliaSimulation.py
@@ -1,7 +1,15 @@
 import random
 import asyncio
 from kademlia.network import Server
+import socket
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    print("Local IP:", ip)
+    s.close()
+    return ip
 
 async def create_and_bootstrap_node(port, bootstrap_node):
     node = Server()
@@ -30,13 +38,14 @@ async def run():
     node_dict = {}
     initial_node = Server()
     await initial_node.listen(8479)
+    ip = get_local_ip()
     port = 8480
     for i in range(100):
         key = i
         value = Server()
         node_dict[key] = value
-        await node_dict[key].listen(port, "192.168.1.233")
-        await node_dict[key].bootstrap([("192.168.1.233", port-1)])
+        await node_dict[key].listen(port, ip)
+        await node_dict[key].bootstrap([(ip, port-1)])
         port += 1
         await node_dict[key].set("key %s" % (key), "value %s" % (key))
     # print("Setting complete")
@@ -54,17 +63,23 @@ async def run():
     # Calculate success rate
     success_count = 0
     total_count = 0
+
     for i in range(100):
+        total_count += 1
+
         try:
             result = await node_dict[0].get("key %s" % (i))
             if result == "value %s" % (i):
                 success_count += 1
-            total_count += 1
+                print(result)
         except Exception as e:
             print("Error fetching value for key {}: {}".format(i, e))
-            total_count += 1
 
     success_rate = success_count / total_count
     print("Success rate: {:.2f}".format(success_rate))
 
+
+
 asyncio.run(run())
+
+
