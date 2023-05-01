f7464089063732040a34342ec628d1d25bc749a5
luoyantao99 <49816287+luoyantao99@users.noreply.github.com>
2023-05-01 12:05:45 -0400

    Update create_nodes.py

--- create_nodes.py
+++ create_nodes.py
@@ -19,7 +19,7 @@ def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     ip = s.getsockname()[0]
-    print("Local IP:", ip)
+    # print("Local IP:", ip)
     s.close()
     return ip
 
